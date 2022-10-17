4ad3bbb70d6ceefe261d5a41de21ad8a8b5634c1
BarkMulcher <lucas.c.haskell@gmail.com>
2022-10-17 17:28:57 -0400

    add some handlebars files & update routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,25 +1,24 @@
-// ALL THE REFERENCES TO BLOG NEED TO BE CHANGED TO WILLS ETC
 
-// const router = require("express").Router();
-// const { Will } = require("../models/");
-// const withAuth = require("../utils/auth");
+const router = require("express").Router();
+const { Will } = require("../models/");
+const withAuth = require("../utils/auth");
 
-// router.get("/", withAuth, (req, res) => {
-//     Will.findAll({
-//       where: {
-//         userId: req.session.userId
-//       }
-//     })
-//       .then(willData => {
-//         const wills = dbBlogData.map((will) => blog.get({ plain: true }));
-//         res.render("all-posts-admin", {
-//           layout: "dashboard",
-//           blogs
-//         });
-//       })
-//       .catch(err => {
-//         console.log(err);
-//         res.redirect("login");
-//       });
-// });
-// module.exports = router;
+router.get("/", withAuth, (req, res) => {
+    Will.findAll({
+      where: {
+        userId: req.session.userId
+      }
+    })
+      .then(willData => {
+        const wills = willData.map((will) => will.get({ plain: true }));
+        res.render("all-posts-admin", {
+          layout: "dashboard",
+          wills
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect('login');
+      });
+});
+module.exports = router;
