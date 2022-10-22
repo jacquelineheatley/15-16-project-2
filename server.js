0bde79f8a217dcc33c8f94b6bc6f50de92c782cf
BarkMulcher <lucas.c.haskell@gmail.com>
2022-10-22 12:20:34 -0400

    cmt fr heroku test

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+app.get('/', function(req, res) {
+    res.render('homepage');
+})
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => 
     console.log(`Now listening at http://localhost:${PORT}.`));
