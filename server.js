1ddbd2142ace19747f342866168889571453a342
BarkMulcher <lucas.c.haskell@gmail.com>
2022-10-22 11:25:07 -0400

    commit for heroku test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+require('dotenv').config();
 
 
 const sequelize = require('./config/connection');
